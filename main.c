e37614411d67ffc98f49e460ba22cfb6582a267d
PerrineLbt <perrinelabarthe1997@gmail.com>
2020-05-07 16:23:55 +0200

    Create main.c
    Fonction Principale

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "astro.h"
+#include <math.h>
+
+double epsilon = 1e-13;
+
+double getSchwarzschild(double masse, double vitesse, int param) {
+    if(masse > ((double)0) && vitesse != ((double)0) && param !=((int)0)){
+        //Exigences dérivées + Rajouter précision en exigence
+        if ((pow( vitesse, ((double)2))) > epsilon) {
+            return (double) (((double) 2) * CONSTANTE_G * masse / (pow( vitesse, ((double)2))));
+        } else {
+            return 0 ;
+        } 
+    } else {
+        return 0;
+    }
+}
